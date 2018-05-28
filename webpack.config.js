bded54e133fab1e98af0e353cf3f537454e68339
Anil Anar <anilanar@hotmail.com>
2018-05-28 19:31:50 +0200

    Remove production build, git-ignore dist directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,16 @@
 var webpack = require("webpack");
-var fileName = "moxios";
-
-if (process.env.NODE_ENV === "production") {
-  fileName += ".min";
-}
 
 module.exports = {
-  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  mode: "development",
   devtool: "source-map",
   entry: "./index.js",
   output: {
-    filename: fileName + ".js",
+    filename: "moxios.js",
     library: "moxios",
     libraryTarget: "umd"
   },
   externals: {
     axios: "axios"
-  },
-  optimization: {
-    minimize: process.env.NODE_ENV === "production"
   },
   module: {
     rules: [
